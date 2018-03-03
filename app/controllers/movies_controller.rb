cdd6ac9513b2c941ede722ca44b22fe0d4280b4b
Liz House <lizehouse@gmail.com>
2018-03-03 13:13:00 -0700

    movies controller update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :movie_params, only: [:show, :create, :edit, :index]
+  before_action :movie_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @movies = Movie.all
