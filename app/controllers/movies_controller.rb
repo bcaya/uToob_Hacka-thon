901b0c3bccefde2bb23a557d0dbc518f6713ef69
Jandrei Pitti <jp.pitti@yahoo.com>
2018-03-03 11:44:14 -0700

    Movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,36 @@
 class MoviesController < ApplicationController
+  before_action :movie_params, :only [:show, :create, :edit]
+
   def index
+    @movies = Movies.all
   end
 
   def show
+    @movie = Movie.find(movie_params)
   end
 
   def new
+    @movie = Movie.new
+    render 'form'
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
   end
 
   def edit
+    @movie = Movie.find(movie_params)
+    render 'form'
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :duration, :genre)
   end
 end
