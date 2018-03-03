7ca173a3f757725def1bb6a17f70361f711c8d2e
Bobby Caya <bob.caya1@gmail.com>
2018-03-03 10:47:22 -0700

    Fix comment controller

--- db/migrate/20180303171746_create_comments.rb
+++ db/migrate/20180303171746_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.belongs_to :User, foreign_key: true
-      t.belongs_to :Movie, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :movie, foreign_key: true
       t.integer :user_id
       t.integer :movie_id
       t.text :body
